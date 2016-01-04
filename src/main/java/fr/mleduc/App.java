fdfe91378413e628624eec64da7868c1f7f9a36c
Manuel Leduc <manuel.leduc@inria.fr>
2016-01-04 12:13:27 +0100

    needed a scm

---
+++ src/main/java/fr/mleduc/App.java
@@ -0,0 +1,17 @@
+package fr.mleduc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+    private static void unused() {
+        System.out.println( "Never called" );
+    }
+}
